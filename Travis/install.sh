c7fd147b8ab46efe2420e1f66f42ea90efc0bebb
Arnd <Zanshin_Github@sv-zanshin.com>
2020-06-10 17:18:32 +0200

    Update install.sh

--- Travis/install.sh
+++ Travis/install.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/env bash
-######################################################################################################
-## Travis Script for building projects                                                              ##
-##                                                                                                  ##
-## Date       Developer                     Comments                                                ##
-## ========== ============================= ======================================================= ##
-## 2020-06-10 https://github.com/SV-Zanshin Cloned from original script written by adafruit         ##
-##                                                                                                  ##
-######################################################################################################
+####################################################################################################################################
+## Bash Script for building projects using Travis-CI.                                                                             ##
+## The script was copied from the ADAFRUIT original version on 2020-06-10 and modified for use in the Zanduino project            ##
+##                                                                                                                                ##
+## Date       Developer                     Comments                                                                              ##
+## ========== ============================= ===================================================================================== ##
+## 2020-06-10 https://github.com/SV-Zanshin Cloned from original script written by adafruit                                       ##
+##                                                                                                                                ##
+####################################################################################################################################
+GRAY='\033[1;30m'
+RED='\033[0;31m'
+LRED='\033[1;31m'
+GREEN='\033[0;32m'
+LGREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LBLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LPURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LCYAN='\033[1;36m'
+LGRAY='\033[0;37m'
+WHITE='\033[1;37m';
+
+echo "${GREEN}Travis-CI${WHITE} build on LINUX, script version ${LGREEN}1.0.0${WHITE}"
+
 # we need bash 4 for associative arrays
 if [ "${BASH_VERSION%%[^0-9]*}" -lt "4" ]; then
   echo "BASH VERSION < 4: ${BASH_VERSION}" >&2
@@ -42,7 +61,6 @@ export DISPLAY=:1.0
 #This condition is to avoid reruning install when build argument is passed
 if [[ $# -eq 0 ]] ; then
 # define colors
-GRAY='\033[1;30m'; RED='\033[0;31m'; LRED='\033[1;31m'; GREEN='\033[0;32m'; LGREEN='\033[1;32m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; LBLUE='\033[1;34m'; PURPLE='\033[0;35m'; LPURPLE='\033[1;35m'; CYAN='\033[0;36m'; LCYAN='\033[1;36m'; LGRAY='\033[0;37m'; WHITE='\033[1;37m';
 
 echo -e "\n########################################################################";
 echo -e "${YELLOW}INSTALLING ARDUINO IDE"
